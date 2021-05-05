2a34d2f960d95b01a30885bf601724e451a19bc0
jessangel79 <babin.angelique@gmail.com>
2021-05-05 15:27:35 +0200

    add imageAnchor

--- MagicPaper/Controller/ViewController.swift
+++ MagicPaper/Controller/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,19 +25,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: Constants.assetsGroupName, bundle: Bundle.main) {
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 1
+//            print("Images Successfully Added")
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -45,30 +49,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    // MARK: - Methods
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        print(imageAnchor.referenceImage.name as Any)
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi/2
+        node.addChildNode(planeNode)
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
---
+++ MagicPaper/Helpers/Extensions/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MagicPaper
+//
+//  Created by Angelique Babin on 05/05/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static let assetsGroupName = "NewsPaperImages"
+}
